f839fa14c8644f8ab0d416fb4ef666432e53b123
IcarusCodes <stoian.alex94@yahoo.com>
2018-05-30 22:43:37 +0300

    Fixed a typo in the POST request of vote method

--- polls/views.py
+++ polls/views.py
@@ -22,7 +22,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.Post['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
@@ -31,4 +31,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
